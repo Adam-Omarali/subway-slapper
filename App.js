2185be6224b131a581bc6974fc933a26f23a259d
David Shan <davidshan20070302@gmail.com>
2023-08-25 19:43:43 -0400

    switch

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import {useState, useEffect } from 'react';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+  const [ {x, y, z}, setData] = useState({ x: 0, y: 0, z: 0});
+
+  useEffect(() => {
+    const subscription = Accelerometer.addListener(setData);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>x: {x}</Text>
+      <Text>y: {y}</Text>
+      <Text>z: {z}</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
